245e08ca996afc1ca58329a9965eef5e1fde78d1
rosaBrokkoli <gustav.loewenherz@posteo.de>
2021-12-04 03:29:50 +0100

    test

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ import PromptSync from "prompt-sync";
 
 let promt = PromptSync()
 
-let antwort = promt('Wie geht es dir? ')
+let antwort = promt('Wie  geht es dir? ')
 
 console.log('Dir geht es: ', antwort)
