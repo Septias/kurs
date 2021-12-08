6f55e260ec424bad4c89e65b90f7bee9da85bed8
rosaBrokkoli <gustav.loewenherz@posteo.de>
2021-12-08 16:40:53 +0100

    test

--- bentik/main.js
+++ bentik/main.js
@@ -7,7 +7,7 @@ function prompt2(frage) {
 }
 
 
-let f1 = "Du wachst alleine in einer Oase auf, umgeben von Palmen, neben einem Teich. Außerhalb der Oase erstreckt sich eine Wüste in alle Richtungen, soweit das Auge reicht. Die Sonne geht gerade unter. Schreibe f um ein Feuer zu machen oder w um etwas zu trinken. "
+let f1 = "Du  wachst alleine in einer Oase auf, umgeben von Palmen, neben einem Teich. Außerhalb der Oase erstreckt sich eine Wüste in alle Richtungen, soweit das Auge reicht. Die Sonne geht gerade unter. Schreibe f um ein Feuer zu machen oder w um etwas zu trinken. "
 f1 = "Du wachst alleine in einer Oase auf, umgeben von Palmen, neben einem Teich. Außerhalb der Oase erstreckt sich eine Wüste in alle Richtungen, soweit das Auge reicht. Die Sonne geht gerade unter. Schreibe f um ein Feuer zu machen oder w um etwas zu trinken. "
 
 function spiel() {
