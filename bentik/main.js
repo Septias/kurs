a734b68c639e40ecf4dfb0f857a11c128838236c
Plsstayfriendly <Bentik@gmx.de>
2021-12-08 14:04:50 +0100

    asas

--- bentik/main.js
+++ bentik/main.js
@@ -1,13 +1,14 @@
 import PromptSync from "prompt-sync";
 
+
 let prompt = PromptSync()
 
-let antwort = prompt('Wie geht es dir? ')
+let a = prompt(`Du wachst alleine in einer Oase auf, 
+umgeben von Palmen, neben einem Teich. 
+Außerhalb der Oase erstreckt sich eine Wüste in alle Richtungen,
+ soweit das Auge reicht. Die Sonne geht gerade unter.
+ Schreibe f um ein Feuer zu machen oder w um etwas zu trinken.`)
 
-console.log('Dir geht es: ', antwort)
-
-let sp = prompt("f für feuer oder s für wasser")
-
-    if(sp == "f"){
-        console.log;("du machst ein feuer");
-    }
+if(a == "f"){
+    console.log("du machst ein feuer")
+}
