0f99a1b080b8688a79f3b169b5d4d2386d6e043d
aimlessb <benedictbrenner06@gmail.com>
2022-02-17 14:49:33 +0100

    moving

--- Browsergames/Benedict/src/main.ts
+++ Browsergames/Benedict/src/main.ts
@@ -5,24 +5,19 @@ import './style.css';
 const _app = new p5(p5Instance => {
   const p = p5Instance as unknown as p5;
 
-  let x = getRndInteger(0,500);
-  let y = getRndInteger(0,500);
-  let r = getRndInteger(0,255)
+  let x = p.windowWidth / 2 - 105
+  let y = p.windowHeight / 2 - 105
+  let moveleft = false
+  let moveright = false
+  /*let r = getRndInteger(0,255)
   let g = getRndInteger(0,255)
   let b = getRndInteger(0,255)
   let r2 = getRndInteger(0,255)
   let g2 = getRndInteger(0,255)
   let b2 = getRndInteger(0,255)
   let q = getRndInteger(0,500);
-  let h = getRndInteger(0,500);
-  
-
-  p.setup = function setup() {
-    p.createCanvas(this.windowWidth, this.windowHeight);
-  };
-
-  p.draw = function draw() {
-    y = getRndInteger(0,500);
+  let h = getRndInteger(0,500);*/
+  /*(y = getRndInteger(0,500);
     x = getRndInteger(0,500);
     r = getRndInteger(0,255)
     g = getRndInteger(0,255)
@@ -31,13 +26,58 @@ const _app = new p5(p5Instance => {
     g2 = getRndInteger(0,255)
     b2 = getRndInteger(0,255)
     h = getRndInteger(0,500);
-    q = getRndInteger(0,500);
-    p.background(r2, g2, b2);
-    p.fill(r, g, b);
-    p.rect(x, y, h, q);
+    q = getRndInteger(0,500);*/
+
+  p.setup = function setup() {
+    p.createCanvas(this.windowWidth, this.windowHeight);
+  };
+
+
+  p.keyPressed = function () {
+    if(p.keyCode === p.LEFT_ARROW) {
+      moveleft = true
+    }
+    
+  }
+  p.keyReleased = function() {
+    if(p.keyCode === p.LEFT_ARROW) {
+      moveleft = false
+    }
+  }
+  p.keyPressed = function () {
+    if(p.keyCode === p.LEFT_ARROW) {
+      moveleft = true
+    }
+    
+  }
+  p.keyReleased = function() {
+    if(p.keyCode === p.LEFT_ARROW) {
+      moveleft = false
+    }
+  }
+
+  p.draw = function draw() {
+    p.background(0, 0, 0);
+    p.fill(2, 200, 200);
+    p.rect(x, y, 210, 210);
+    if(moveleft == true){
+      x=x-2
+      console.log(x);
+      if(x<=-300){
+        x=this.windowWidth
+      } 
+    }
+    if(moveleft == true){
+      x=x-2
+      console.log(x);
+      if(x<=-300){
+        x=this.windowWidth
+      } 
+    }
   };
 }, document.getElementById('app')!);
 
-function getRndInteger(min, max) {
+function getRndInteger(min:any, max:any) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
+                                
