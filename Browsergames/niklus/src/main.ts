96571c9ae67d407b66c36d04ad3fe4f74a39fd4f
ObamaGamerGirl69 <linus@koetzle.net>
2022-02-17 14:37:35 +0100

    NiklasBastelkiste

--- Browsergames/niklus/src/main.ts
+++ Browsergames/niklus/src/main.ts
@@ -5,17 +5,26 @@ import './style.css';
 const _app = new p5(p5Instance => {
   const p = p5Instance as unknown as p5;
 
-  const x = 50;
-  const y = 450;
+  const x = 150;
+  const y = 350;
 
   p.setup = function setup() {
     p.createCanvas(500, 500);
   };
 
-  p.draw = function draw() {
-    p.background(0);
-    p.fill(51, 25,0);
-    p.rect(x, y, 50, 50);
+  let img = p.loadImage("bilder/bild.jpg")
+  p.draw = () => {
+    //p.background(0);
+    //p.fill(58, 25, 70);
+    //p.rect(x, y, 50, 700);
+    p.image(img, 0, 0)
+
+  if
+
   };
 }, document.getElementById('app')!);
+  
+
+ 
+ 
 
