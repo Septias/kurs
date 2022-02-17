97e952c26c9a3fa7f4e3926dc3a8b42483bb365e
Blaze-Codes <nic.holzmann@web.de>
2022-02-17 15:02:30 +0100

    hhju

--- Browsergames/Nicolas/src/main.ts
+++ Browsergames/Nicolas/src/main.ts
@@ -2,11 +2,13 @@ import p5 from 'p5';
 
 import './style.css';
 
-const _app = new p5(p5Instance => {
+var achseX = 50;
+var achseY = 50;
+
+const app = new p5(p5Instance => {
   const p = p5Instance as unknown as p5;
 
-  const x = 300;
-  const y = 300;
+
 
   p.setup = function setup() {
     p.createCanvas(500, 500);
@@ -14,38 +16,37 @@ const _app = new p5(p5Instance => {
   p.keyPressed = () => {
     //console.log (p.keyCode);
 
-   
+
 
 
 
   };
   p.draw = function draw() {
 
-    var achseX = 50;
-    var achseY = 50;
 
-      if (p.keyIsDown(87) || p.keyIsDown(38)) {
+
+    if (p.keyIsDown(87) || p.keyIsDown(38)) {
       console.log("W");
-      achseY = achseY +1;
+      achseY = achseY + 1;
     }
 
     if (p.keyIsDown(65) || p.keyIsDown(37)) {
       console.log("A");
-      achseX = achseX -1;
+      achseX = achseX - 1;
     }
 
     if (p.keyIsDown(83) || p.keyIsDown(40)) {
       console.log("S");
-      achseY = achseY -1;
+      achseY = achseY - 1;
     }
 
     if (p.keyIsDown(87) || p.keyIsDown(39)) {
       console.log("D");
-      achseX = achseX +1;
+      achseX = achseX + 1;
     }
-    
+
     p.keyIsDown(87)
-    
+
     p.background(0);
     p.fill(205);
     p.rect(achseX, achseY, 50, 50);
