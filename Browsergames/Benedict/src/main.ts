1828f47e6822a55cfba32ef4fc99c770ea24ef5b
rosaBrokkoli <gustav.loewenherz@posteo.de>
2022-02-18 20:37:08 +0100

    pac man without bugdet

--- Browsergames/Benedict/src/main.ts
+++ Browsergames/Benedict/src/main.ts
@@ -1,23 +1,35 @@
 import p5 from 'p5';
-
 import './style.css';
 
 const _app = new p5(p5Instance => {
   const p = p5Instance as unknown as p5;
 
-  
 
-  let object_width = 300
-  let object_hight = 100
+  //Player Dimension
+  let player_width = 150
+  let player_height = 150
+
+  //Player Speed
+  let speed = 15
+
+  //Player Positional Variables
+  let player_position_horizontal = p.windowWidth / 2 - player_width / 2
+  let player_position_vertical = p.windowHeight / 2 - player_height / 2
+  let player_runoff_width = 0 - player_width
+  let player_runoff_height = 0 - player_height
+
+  //Collectible Dimensions
+  let collectible_width = 50
+  let collectible_height = 50
 
 
-  let speed = 4
+  //Collectible Positional Variables
+  let collectible_spawn_area_horizontal = p.windowWidth - collectible_width / 2
+  let collectible_spawn_area_vertical = p.windowHeight - collectible_height / 2
+  let collectible_position_horizontal = getRndInteger(0, collectible_spawn_area_horizontal)
+  let collectible_position_vertical = getRndInteger(0, collectible_spawn_area_vertical)
 
-  let object_position_horizontal = p.windowWidth / 2 - object_width / 2
-  let object_position_vertical = p.windowHeight / 2 - object_hight / 2
-  let object_runoff_width = 0 - object_width
-  let object_runoff_hight = 0 - object_hight
-
+  //Move Variabels
   let moveleft = false
   let moveright = false
   let moveup = false
@@ -27,7 +39,12 @@ const _app = new p5(p5Instance => {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
 
+  //Random Integer
+  function getRndInteger(min:any, max:any) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
+  //Handles All Keypresses
   p.keyPressed = function () {
     if (p.keyCode === p.LEFT_ARROW) {
       moveleft = true
@@ -41,8 +58,10 @@ const _app = new p5(p5Instance => {
     if (p.keyCode === p.DOWN_ARROW) {
       movedown = true
     }
-    
+
   }
+
+  //Handles All Keyrealeses
   p.keyReleased = function () {
     if (p.keyCode === p.LEFT_ARROW) {
       moveleft = false
@@ -62,44 +81,58 @@ const _app = new p5(p5Instance => {
 
   p.draw = function draw() {
     p.background(0, 0, 0);
+    
+    //Collectible
+    p.fill(255,0,0)
+    p.rect(collectible_position_horizontal, collectible_position_vertical, collectible_width, collectible_height);
+    
+    //Player
     p.fill(2, 200, 200);
-    p.rect(object_position_horizontal, object_position_vertical, object_width, object_hight);
+    p.rect(player_position_horizontal, player_position_vertical, player_width, player_height);
+
+    //Collectible Position Debug
+    //console.log(collectible_position_horizontal, collectible_position_vertical);
+
+
+    //Movement Checks
     if (moveleft == true) {
-      object_position_horizontal = object_position_horizontal - speed
-      console.log(object_position_horizontal);
-      if (object_position_horizontal <= object_runoff_width) {
-        object_position_horizontal = p.windowWidth
+      player_position_horizontal = player_position_horizontal - speed
+      //console.log(player_position_horizontal);
+      if (player_position_horizontal <= player_runoff_width) {
+        player_position_horizontal = p.windowWidth
       }
     }
     if (moveright == true) {
-      object_position_horizontal = object_position_horizontal + speed
-      console.log(object_position_horizontal);
-      if (object_position_horizontal >= p.windowWidth) {
-        object_position_horizontal = object_runoff_width
+      player_position_horizontal = player_position_horizontal + speed
+      //console.log(player_position_horizontal);
+      if (player_position_horizontal >= p.windowWidth) {
+        player_position_horizontal = player_runoff_width
       }
     }
     if (moveup == true) {
-      object_position_vertical = object_position_vertical - speed
-      console.log(object_position_vertical);
-      if (object_position_vertical <= object_runoff_hight) {
-        object_position_vertical = p.windowHeight
+      player_position_vertical = player_position_vertical - speed
+      //console.log(player_position_vertical);
+      if (player_position_vertical <= player_runoff_height) {
+        player_position_vertical = p.windowHeight
       }
     }
     if (movedown == true) {
-      object_position_vertical = object_position_vertical + speed
-      console.log(object_position_vertical);
-      if (object_position_vertical >= p.windowHeight) {
-        object_position_vertical = object_runoff_hight
+      player_position_vertical = player_position_vertical + speed
+      //console.log(player_position_vertical);
+      if (player_position_vertical >= p.windowHeight) {
+        player_position_vertical = player_runoff_height
+      }
+    }
+
+    //Collectible Checks
+    if (player_position_horizontal + player_width >= collectible_position_horizontal + collectible_width && player_position_horizontal - player_width <= collectible_position_horizontal - collectible_width) {
+      if (player_position_vertical + player_height >= collectible_position_vertical + collectible_height && player_position_vertical - player_height <= collectible_position_vertical - collectible_height) {
+
+        collectible_position_horizontal = getRndInteger(0, collectible_spawn_area_horizontal)
+        collectible_position_vertical = getRndInteger(0, collectible_spawn_area_vertical)
+
+        console.log("hit");
       }
     }
   };
-
-
-
-
 }, document.getElementById('app')!);
-
-
-
-
-
