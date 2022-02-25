c9bd35a0636866ff39516d6e4c770f599ac8c265
Plsstayfriendly <Bentik@gmx.de>
2022-02-25 15:56:52 +0100

    aufgeräumt 2

--- Browsergames/snake/src/main.ts
+++ Browsergames/snake/src/main.ts
@@ -1,41 +1,48 @@
-import p5, { Vector } from 'p5';
+import p5 from 'p5';
+
 import './style.css';
-
-//Obstacles
-let speed_obstackle = 4
-let spawntrigger = true
-let obstacle1_width = 35
-let obstacle1_height = 35
-
-//Randomizer
-function getRndInteger(min: any, max: any) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 const _app = new p5(p5Instance => {
   const p = p5Instance as unknown as p5;
 
-  //Background  
-  p.setup = function setup() {
-    p.createCanvas(p.windowWidth, p.windowHeight)
-    p.textFont("Calibri")
-  }
-
   //Deathscreen
   let deathscreen_width = p.windowWidth
-  let deathscreen_height = p.windowHeight
+  let deathscreen_hight = p.windowHeight
   let deathscreen_pos_horizontal = 0
   let deathscreen_pos_vertikal = 0
   let space = false
 
   //Player
   let Player_Width = 90
-  let Player_height = 90
+  let Player_Hight = 90
   let Player_Position_Horizontal = p.windowWidth / 2 - Player_Width / 2
-  let Player_Position_Vertikal = p.windowHeight / 2 - Player_height / 2
+  let Player_Position_Vertikal = p.windowHeight / 2 - Player_Hight / 2
   let lives = 3
   let speed = 6
   let up = false
+
+  //Obstacles
+  let speedobstackle = 3
+  let obstacle1_width = 35
+  let obstacle1_hight = 35
+  let spawntrigger = true
+
+  let obstacle_spawn_position_horizontal = p.windowWidth + obstacle1_width
+  let obstacle_spawn_area_vertical = p.windowHeight - obstacle1_hight / 2
+  let obstacle_pos_horizontal = obstacle_spawn_position_horizontal
+  let obstacle_pos_vertical = getRndInteger(0 - obstacle1_hight / 2, obstacle_spawn_area_vertical)
+
+  //Randomizer
+  function getRndInteger(min: any, max: any) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  //Background  
+  p.setup = function setup() {
+    p.createCanvas(p.windowWidth, p.windowHeight,)
+
+  }
+  //let img = p.loadImage("https://tse4.mm.bing.net/th?id=OIP.3Ig2q6rJvm_RRlAHZathMAHaEW&pid=Api")
 
   //Keys
   p.keyPressed = function () {
@@ -55,116 +62,73 @@ const _app = new p5(p5Instance => {
     }
   }
 
-  let obstacles: Obstacle[] = []
-
   //Draw
   p.draw = function draw() {
+
     if (spawntrigger == true) {
-      //obstacle_pos_vertical = getRndInteger(0 - obstacle1_height / 2, obstacle_spawn_area_vertical)
-      obstacles.push(createObstacle(p.windowHeight, p.windowWidth))
+      obstacle_pos_vertical = getRndInteger(0 - obstacle1_hight / 2, obstacle_spawn_area_vertical)
     }
 
     p.background(0, 0, 0)//img für bild
+    p.textFont("Calibri")
     p.textSize(20)
     p.fill(255, 255, 255)
     p.textAlign(p.LEFT)
     p.text("Lives: " + lives, 25, 35)
 
-    console.log('Objektanzahl: ', obstacles.length);
-
     //Player
     p.fill(0, 200, 250)
-    p.rect(Player_Position_Horizontal, Player_Position_Vertikal, Player_Width, Player_height)
+    p.rect(Player_Position_Horizontal, Player_Position_Vertikal, Player_Width, Player_Hight)
 
     if (up == true) {
       Player_Position_Vertikal = Player_Position_Vertikal - speed
     }
-
     if (up == false) {
       Player_Position_Vertikal = Player_Position_Vertikal + speed
     }
-
-    if (Player_Position_Vertikal > p.windowHeight - Player_height) {
-      Player_Position_Vertikal = p.windowHeight - Player_height
+    if (Player_Position_Vertikal > p.windowHeight - Player_Hight) {
+      Player_Position_Vertikal = p.windowHeight - Player_Hight
     }
-
     if (Player_Position_Vertikal < 0) {
       Player_Position_Vertikal = 0
     }
 
     //Obstacles
-    p.fill(255, 50, 30)
-
-    //p.rect(obstacle_pos_horizontal, obstacle_pos_vertical, obstacle1_width, obstacle1_height)
-    // obstacle_pos_horizontal = obstacle_pos_horizontal - speed_obstackle
-
-    for (const obstacle of obstacles) {
-      obstacle.update()
-      obstacle.draw(p)
-      if (obstacle.collide(Player_Position_Horizontal, Player_Position_Vertikal, Player_Width, Player_height)) {
-
-      }
+    p.fill(600, 50, 30)
+    p.rect(obstacle_pos_horizontal, obstacle_pos_vertical, obstacle1_width, obstacle1_hight)
+    obstacle_pos_horizontal = obstacle_pos_horizontal - speedobstackle
+    if (obstacle_pos_horizontal > 0) {
+      spawntrigger = false
+    }
+    if (obstacle_pos_horizontal < 0) {
+      obstacle_pos_horizontal = p.windowWidth + obstacle1_width
+      spawntrigger = true
     }
 
     //Damage
-    /*if (Player_Position_Horizontal + Player_Width >= obstacle_pos_horizontal + obstacle1_width && Player_Position_Horizontal <= obstacle_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height >= obstacle_pos_vertical + obstacle1_height && Player_Position_Vertikal <= obstacle_pos_vertical) {
-
-        spawntrigger = true
-
+    if (Player_Position_Horizontal + Player_Width >= obstacle_pos_horizontal + obstacle1_width && Player_Position_Horizontal <= obstacle_pos_horizontal) {
+      if (Player_Position_Vertikal + Player_Hight >= obstacle_pos_vertical + obstacle1_hight && Player_Position_Vertikal <= obstacle_pos_vertical) {
         obstacle_pos_horizontal = p.windowWidth + obstacle1_width
-
         lives = lives - 1
-
+        spawntrigger = true
         console.log("Damage")
         console.log(lives)
       }
-    }*/
+    }
 
     //Death
     if (lives == 0) {
       p.fill(0, 0, 0)
-      p.rect(deathscreen_pos_horizontal, deathscreen_pos_vertikal, deathscreen_width, deathscreen_height)
+      p.rect(deathscreen_pos_horizontal, deathscreen_pos_vertikal, deathscreen_width, deathscreen_hight)
       p.fill(600, 50, 30)
       p.textSize(90)
       p.textAlign(p.CENTER)
       p.text("You Died", p.windowWidth / 2, p.windowHeight / 2)
-
       if (space == true) {
         lives = 3
       }
     }
+
   }
+
 }, document.getElementById('app')!);
-class Obstacle {
-  constructor(private x: number, private y: number, private w: number, private h: number, private speed: number) {
-
-  }
-
-  draw(p: p5) {
-    p.fill(0, 125, 21)
-    p.rect(this.x, this.y, this.w, this.h)
-  }
-
-  update() {
-    this.x -= this.speed
-  }
-
-  collide(x_other: number, y_other: number, w_other: number, h_other: number): boolean {
-    if (x_other + w_other >= this.x + this.w && x_other <= this.x) {
-      if (y_other + h_other >= this.y + this.h && y_other <= this.y) {
-
-        return true
-      }
-    }
-    return false
-  }
-}
-
-function createObstacle(windowHeight: number, windowWidth: number) {
-  let obstacle_spawn_area_vertical = windowHeight - obstacle1_height / 2
-  let x = windowWidth + obstacle1_width
-  let y = getRndInteger(0 - obstacle1_height / 2, obstacle_spawn_area_vertical)
-
-  return new Obstacle(x, y, 20, 20, speed_obstackle)
-}
