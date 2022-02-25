4ad7e45b6ec979c99a28038c8ec8d3becd198547
Plsstayfriendly <Bentik@gmx.de>
2022-02-25 18:14:01 +0100

    menu broken

--- Browsergames/snake/src/main.ts
+++ Browsergames/snake/src/main.ts
@@ -5,12 +5,71 @@ import './style.css';
 const _app = new p5(p5Instance => {
   const p = p5Instance as unknown as p5;
 
+
+  //Obstacle 1
+  let obstacle1_width = 35
+  let obstacle1_hight = 35
+  let spawntrigger1 = true
+  let rnd_number1 = p.random(200)
+  let rnd_speed1 = p.random(3, 5)
+  let obstacle1_spawn_pos_horizontal = p.windowWidth + obstacle1_width + rnd_number1 * 2
+  let obstacle1_spawn_area_vertical = p.windowHeight - obstacle1_hight / 2
+  let obstacle1_pos_horizontal = obstacle1_spawn_pos_horizontal
+  let obstacle1_pos_vertical = getRndInteger(0 - obstacle1_hight / 2, obstacle1_spawn_area_vertical)
+
+  //Obstacle 2
+  let obstacle2_width = 35
+  let obstacle2_hight = 35
+  let spawntrigger2 = true
+  let rnd_number2 = p.random(200)
+  let rnd_speed2 = p.random(3, 5)
+  let obstacle2_spawn_pos_horizontal = p.windowWidth + obstacle2_width + rnd_number2 * 2
+  let obstacle2_spawn_area_vertical = p.windowHeight - obstacle2_hight / 2
+  let obstacle2_pos_horizontal = obstacle2_spawn_pos_horizontal
+  let obstacle2_pos_vertical = getRndInteger(0 - obstacle2_hight / 2, obstacle2_spawn_area_vertical)
+
+  //Obstacle 3
+  let obstacle3_width = 35
+  let obstacle3_hight = 35
+  let spawntrigger3 = true
+  let rnd_number3 = p.random(200)
+  let rnd_speed3 = p.random(3, 5)
+  let obstacle3_spawn_pos_horizontal = p.windowWidth + obstacle3_width + rnd_number3 * 2
+  let obstacle3_spawn_area_vertical = p.windowHeight - obstacle3_hight / 2
+  let obstacle3_pos_horizontal = obstacle3_spawn_pos_horizontal
+  let obstacle3_pos_vertical = getRndInteger(0 - obstacle3_hight / 2, obstacle3_spawn_area_vertical)
+
+  //Obstacle 4
+  let obstacle4_width = 35
+  let obstacle4_hight = 35
+  let spawntrigger4 = true
+  let rnd_number4 = p.random(200)
+  let rnd_speed4 = p.random(3, 5)
+  let obstacle4_spawn_pos_horizontal = p.windowWidth + obstacle4_width + rnd_number4 * 2
+  let obstacle4_spawn_area_vertical = p.windowHeight - obstacle4_hight / 2
+  let obstacle4_pos_horizontal = obstacle4_spawn_pos_horizontal
+  let obstacle4_pos_vertical = getRndInteger(0 - obstacle4_hight / 2, obstacle4_spawn_area_vertical)
+
+  //Obstacle 5
+  let obstacle5_width = 35
+  let obstacle5_hight = 35
+  let spawntrigger5 = true
+  let rnd_number5 = p.random(200)
+  let rnd_speed5 = p.random(3, 5)
+  let obstacle5_spawn_pos_horizontal = p.windowWidth + obstacle5_width + rnd_number5 * 2
+  let obstacle5_spawn_area_vertical = p.windowHeight - obstacle5_hight / 2
+  let obstacle5_pos_horizontal = obstacle5_spawn_pos_horizontal
+  let obstacle5_pos_vertical = getRndInteger(0 - obstacle5_hight / 2, obstacle5_spawn_area_vertical)
+
   //Deathscreen
   let deathscreen_width = p.windowWidth
   let deathscreen_hight = p.windowHeight
   let deathscreen_pos_horizontal = 0
   let deathscreen_pos_vertikal = 0
   let space = false
+
+  //Menu
+  let menu = false
 
   //Player
   let Player_Width = 90
@@ -20,17 +79,8 @@ const _app = new p5(p5Instance => {
   let lives = 3
   let speed = 6
   let up = false
+  let hit = false
 
-  //Obstacles
-  let speedobstackle = 3
-  let obstacle1_width = 35
-  let obstacle1_hight = 35
-  let spawntrigger = true
-
-  let obstacle_spawn_position_horizontal = p.windowWidth + obstacle1_width
-  let obstacle_spawn_area_vertical = p.windowHeight - obstacle1_hight / 2
-  let obstacle_pos_horizontal = obstacle_spawn_position_horizontal
-  let obstacle_pos_vertical = getRndInteger(0 - obstacle1_hight / 2, obstacle_spawn_area_vertical)
 
   //Randomizer
   function getRndInteger(min: any, max: any) {
@@ -51,6 +101,9 @@ const _app = new p5(p5Instance => {
     if (p.keyCode === 32) {
       space = true
     }
+    if (p.keyCode === 27) {
+      menu = true
+    }
   }
 
   p.keyReleased = function () {
@@ -60,25 +113,43 @@ const _app = new p5(p5Instance => {
     if (p.keyCode === 32) {
       space = false
     }
+    if (p.keyCode === 27)
+    menu = false
   }
+
+
 
   //Draw
   p.draw = function draw() {
 
-    if (spawntrigger == true) {
-      obstacle_pos_vertical = getRndInteger(0 - obstacle1_hight / 2, obstacle_spawn_area_vertical)
+    //Spawn
+    if (spawntrigger1 == true) {
+      obstacle1_pos_vertical = getRndInteger(0 - obstacle1_hight / 2, obstacle1_spawn_area_vertical)
+    }
+    if (spawntrigger2 == true) {
+      obstacle2_pos_vertical = getRndInteger(0 - obstacle1_hight / 2, obstacle1_spawn_area_vertical)
+    }
+    if (spawntrigger3 == true) {
+      obstacle3_pos_vertical = getRndInteger(0 - obstacle1_hight / 2, obstacle1_spawn_area_vertical)
+    }
+    if (spawntrigger4 == true) {
+      obstacle4_pos_vertical = getRndInteger(0 - obstacle1_hight / 2, obstacle1_spawn_area_vertical)
+    }
+    if (spawntrigger5 == true) {
+      obstacle5_pos_vertical = getRndInteger(0 - obstacle1_hight / 2, obstacle1_spawn_area_vertical)
     }
 
     p.background(0, 0, 0)//img für bild
-    p.textFont("Calibri")
-    p.textSize(20)
-    p.fill(255, 255, 255)
-    p.textAlign(p.LEFT)
-    p.text("Lives: " + lives, 25, 35)
 
     //Player
-    p.fill(0, 200, 250)
+    if (hit == true) {
+      p.fill(255, 215, 0)
+    }
+    if (hit == false) {
+      p.fill(0, 200, 250)
+    }
     p.rect(Player_Position_Horizontal, Player_Position_Vertikal, Player_Width, Player_Hight)
+
 
     if (up == true) {
       Player_Position_Vertikal = Player_Position_Vertikal - speed
@@ -93,31 +164,151 @@ const _app = new p5(p5Instance => {
       Player_Position_Vertikal = 0
     }
 
-    //Obstacles
+    //Obstacle 1
     p.fill(600, 50, 30)
-    p.rect(obstacle_pos_horizontal, obstacle_pos_vertical, obstacle1_width, obstacle1_hight)
-    obstacle_pos_horizontal = obstacle_pos_horizontal - speedobstackle
-    if (obstacle_pos_horizontal > 0) {
-      spawntrigger = false
+    p.rect(obstacle1_pos_horizontal, obstacle1_pos_vertical, obstacle1_width, obstacle1_hight)
+    obstacle1_pos_horizontal = obstacle1_pos_horizontal - rnd_speed1
+    if (obstacle1_pos_horizontal > 0) {
+      spawntrigger1 = false
     }
-    if (obstacle_pos_horizontal < 0) {
-      obstacle_pos_horizontal = p.windowWidth + obstacle1_width
-      spawntrigger = true
+    if (obstacle1_pos_horizontal < 0) {
+      obstacle1_pos_horizontal = p.windowWidth + obstacle1_width
+      spawntrigger1 = true
     }
 
-    //Damage
-    if (Player_Position_Horizontal + Player_Width >= obstacle_pos_horizontal + obstacle1_width && Player_Position_Horizontal <= obstacle_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_Hight >= obstacle_pos_vertical + obstacle1_hight && Player_Position_Vertikal <= obstacle_pos_vertical) {
-        obstacle_pos_horizontal = p.windowWidth + obstacle1_width
+    //Obstacle 2
+    p.fill(600, 50, 30)
+    p.rect(obstacle2_pos_horizontal, obstacle2_pos_vertical, obstacle2_width, obstacle2_hight)
+    obstacle2_pos_horizontal = obstacle2_pos_horizontal - rnd_speed2
+    if (obstacle2_pos_horizontal > 0) {
+      spawntrigger2 = false
+    }
+    if (obstacle2_pos_horizontal < 0) {
+      obstacle2_pos_horizontal = p.windowWidth + obstacle2_width
+      spawntrigger2 = true
+    }
+
+    //Obstacle 3
+    p.fill(600, 50, 30)
+    p.rect(obstacle3_pos_horizontal, obstacle3_pos_vertical, obstacle3_width, obstacle3_hight)
+    obstacle3_pos_horizontal = obstacle3_pos_horizontal - rnd_speed3
+    if (obstacle3_pos_horizontal > 0) {
+      spawntrigger3 = false
+    }
+    if (obstacle3_pos_horizontal < 0) {
+      obstacle3_pos_horizontal = p.windowWidth + obstacle3_width
+      spawntrigger3 = true
+    }
+
+    //Obstacle 4
+    p.fill(600, 50, 30)
+    p.rect(obstacle4_pos_horizontal, obstacle4_pos_vertical, obstacle4_width, obstacle4_hight)
+    obstacle4_pos_horizontal = obstacle4_pos_horizontal - rnd_speed4
+    if (obstacle4_pos_horizontal > 0) {
+      spawntrigger4 = false
+    }
+    if (obstacle4_pos_horizontal < 0) {
+      obstacle4_pos_horizontal = p.windowWidth + obstacle4_width
+      spawntrigger4 = true
+    }
+
+    //Obstacle 5
+    p.fill(600, 50, 30)
+    p.rect(obstacle5_pos_horizontal, obstacle5_pos_vertical, obstacle5_width, obstacle5_hight)
+    obstacle5_pos_horizontal = obstacle5_pos_horizontal - rnd_speed5
+    if (obstacle5_pos_horizontal > 0) {
+      spawntrigger5 = false
+    }
+    if (obstacle5_pos_horizontal < 0) {
+      obstacle5_pos_horizontal = p.windowWidth + obstacle5_width
+      spawntrigger5 = true
+    }
+
+    //Damage 1
+    if (Player_Position_Horizontal + Player_Width > obstacle1_pos_horizontal + obstacle1_width && Player_Position_Horizontal < obstacle1_pos_horizontal) {
+      if (Player_Position_Vertikal + Player_Hight > obstacle1_pos_vertical + obstacle1_hight && Player_Position_Vertikal < obstacle1_pos_vertical) {
+        obstacle1_pos_horizontal = p.windowWidth + obstacle1_width
         lives = lives - 1
-        spawntrigger = true
+        spawntrigger1 = true
+        hit = true
         console.log("Damage")
         console.log(lives)
       }
     }
 
+    //Damage 2
+    if (Player_Position_Horizontal + Player_Width > obstacle2_pos_horizontal + obstacle2_width && Player_Position_Horizontal < obstacle2_pos_horizontal) {
+      if (Player_Position_Vertikal + Player_Hight > obstacle2_pos_vertical + obstacle2_hight && Player_Position_Vertikal < obstacle2_pos_vertical) {
+        obstacle2_pos_horizontal = p.windowWidth + obstacle2_width
+        lives = lives - 1
+        spawntrigger2 = true
+        hit = true
+        console.log("Damage")
+        console.log(lives)
+      }
+    }
+
+    //Damage 3
+    if (Player_Position_Horizontal + Player_Width > obstacle3_pos_horizontal + obstacle3_width && Player_Position_Horizontal < obstacle3_pos_horizontal) {
+      if (Player_Position_Vertikal + Player_Hight > obstacle3_pos_vertical + obstacle3_hight && Player_Position_Vertikal < obstacle3_pos_vertical) {
+        obstacle3_pos_horizontal = p.windowWidth + obstacle3_width
+        lives = lives - 1
+        spawntrigger3 = true
+        hit = true
+        console.log("Damage")
+        console.log(lives)
+      }
+    }
+
+    //Damage 4
+    if (Player_Position_Horizontal + Player_Width > obstacle4_pos_horizontal + obstacle4_width && Player_Position_Horizontal < obstacle4_pos_horizontal) {
+      if (Player_Position_Vertikal + Player_Hight > obstacle4_pos_vertical + obstacle4_hight && Player_Position_Vertikal < obstacle4_pos_vertical) {
+        obstacle4_pos_horizontal = p.windowWidth + obstacle4_width
+        lives = lives - 1
+        spawntrigger4 = true
+        hit = true
+        console.log("Damage")
+        console.log(lives)
+      }
+    }
+
+    //Damage 5
+    if (Player_Position_Horizontal + Player_Width > obstacle5_pos_horizontal + obstacle5_width && Player_Position_Horizontal < obstacle5_pos_horizontal) {
+      if (Player_Position_Vertikal + Player_Hight > obstacle5_pos_vertical + obstacle5_hight && Player_Position_Vertikal < obstacle5_pos_vertical) {
+        obstacle5_pos_horizontal = p.windowWidth + obstacle5_width
+        lives = lives - 1
+        spawntrigger5 = true
+        hit = true
+        console.log("Damage")
+        console.log(lives)
+      }
+    }
+
+
+    //Text
+    p.textFont("Calibri")
+    p.textSize(20)
+    p.fill(255, 255, 255)
+    p.textAlign(p.LEFT)
+    p.text("Lives: " + lives, 25, 55)
+    p.text("Press [ESC] for menu", 25, 30)
+
+    //Menu
+    if (menu == true) {
+      speed = 0
+      rnd_speed1 = 0
+      rnd_speed2 = 0
+      rnd_speed3 = 0
+      rnd_speed4 = 0
+      rnd_speed5 = 0
+      p.textSize(40)
+      p.fill(255,255,255)
+      p.textAlign(p.CENTER)
+      p.text("MENU", p.windowWidth / 2, p.windowHeight / 2 - 100)
+    }
+
     //Death
-    if (lives == 0) {
+    if (lives < 1) {
       p.fill(0, 0, 0)
       p.rect(deathscreen_pos_horizontal, deathscreen_pos_vertikal, deathscreen_width, deathscreen_hight)
       p.fill(600, 50, 30)
