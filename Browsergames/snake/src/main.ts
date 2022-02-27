cc5b23fd799a0cc5efda1ab1d19368c1979bd147
Plsstayfriendly <Bentik@gmx.de>
2022-02-27 17:53:48 +0100

    progress

--- Browsergames/snake/src/main.ts
+++ Browsergames/snake/src/main.ts
@@ -9,7 +9,7 @@ const _app = new p5(p5Instance => {
   let obstacle1_width = 35
   let obstacle1_height = 35
   let spawntrigger1 = true
-  let rnd_number1 = p.random(200)
+  let rnd_number1 = p.random(300)
   let speed1 = 5
   let obstacle1_spawn_pos_horizontal = p.windowWidth + obstacle1_width + rnd_number1 * 2
   let obstacle1_spawn_area_vertical = p.windowHeight - obstacle1_height / 2
@@ -19,7 +19,7 @@ const _app = new p5(p5Instance => {
   let obstacle2_width = 35
   let obstacle2_height = 35
   let spawntrigger2 = true
-  let rnd_number2 = p.random(200)
+  let rnd_number2 = p.random(300)
   let speed2 = 5
   let obstacle2_spawn_pos_horizontal = p.windowWidth + obstacle2_width + rnd_number2 * 2
   let obstacle2_spawn_area_vertical = p.windowHeight - obstacle2_height / 2
@@ -29,7 +29,7 @@ const _app = new p5(p5Instance => {
   let obstacle3_width = 35
   let obstacle3_height = 35
   let spawntrigger3 = true
-  let rnd_number3 = p.random(200)
+  let rnd_number3 = p.random(300)
   let speed3 = 5
   let obstacle3_spawn_pos_horizontal = p.windowWidth + obstacle3_width + rnd_number3 * 2
   let obstacle3_spawn_area_vertical = p.windowHeight - obstacle3_height / 2
@@ -39,7 +39,7 @@ const _app = new p5(p5Instance => {
   let obstacle4_width = 35
   let obstacle4_height = 35
   let spawntrigger4 = true
-  let rnd_number4 = p.random(200)
+  let rnd_number4 = p.random(300)
   let speed4 = 5
   let obstacle4_spawn_pos_horizontal = p.windowWidth + obstacle4_width + rnd_number4 * 2
   let obstacle4_spawn_area_vertical = p.windowHeight - obstacle4_height / 2
@@ -49,7 +49,7 @@ const _app = new p5(p5Instance => {
   let obstacle5_width = 35
   let obstacle5_height = 35
   let spawntrigger5 = true
-  let rnd_number5 = p.random(200)
+  let rnd_number5 = p.random(300)
   let speed5 = 5
   let obstacle5_spawn_pos_horizontal = p.windowWidth + obstacle5_width + rnd_number5 * 2
   let obstacle5_spawn_area_vertical = p.windowHeight - obstacle5_height / 2
@@ -59,7 +59,7 @@ const _app = new p5(p5Instance => {
   let obstacle6_width = 35
   let obstacle6_height = 35
   let spawntrigger6 = true
-  let rnd_number6 = p.random(200, 300)
+  let rnd_number6 = p.random(300, 400)
   let speed6 = 6
   let obstacle6_spawn_pos_horizontal = p.windowWidth + obstacle6_width + rnd_number6 * 2
   let obstacle6_spawn_area_vertical = p.windowHeight - obstacle6_height / 2
@@ -69,7 +69,7 @@ const _app = new p5(p5Instance => {
   let obstacle7_width = 35
   let obstacle7_height = 35
   let spawntrigger7 = true
-  let rnd_number7 = p.random(200, 300)
+  let rnd_number7 = p.random(300, 400)
   let speed7 = 5
   let obstacle7_spawn_pos_horizontal = p.windowWidth + obstacle7_width + rnd_number7 * 2
   let obstacle7_spawn_area_vertical = p.windowHeight - obstacle7_height / 2
@@ -79,7 +79,7 @@ const _app = new p5(p5Instance => {
   let obstacle8_width = 35
   let obstacle8_height = 35
   let spawntrigger8 = true
-  let rnd_number8 = p.random(200, 300)
+  let rnd_number8 = p.random(300, 400)
   let speed8 = 5
   let obstacle8_spawn_pos_horizontal = p.windowWidth + obstacle8_width + rnd_number8 * 2
   let obstacle8_spawn_area_vertical = p.windowHeight - obstacle8_height / 2
@@ -89,7 +89,7 @@ const _app = new p5(p5Instance => {
   let obstacle9_width = 35
   let obstacle9_height = 35
   let spawntrigger9 = true
-  let rnd_number9 = p.random(200, 300)
+  let rnd_number9 = p.random(300, 400)
   let speed9 = 5
   let obstacle9_spawn_pos_horizontal = p.windowWidth + obstacle9_width + rnd_number9 * 2
   let obstacle9_spawn_area_vertical = p.windowHeight - obstacle9_height / 2
@@ -99,7 +99,7 @@ const _app = new p5(p5Instance => {
   let obstacle10_width = 35
   let obstacle10_height = 35
   let spawntrigger10 = true
-  let rnd_number10 = p.random(200, 300)
+  let rnd_number10 = p.random(300, 400)
   let speed10 = 5
   let obstacle10_spawn_pos_horizontal = p.windowWidth + obstacle10_width + rnd_number10 * 2
   let obstacle10_spawn_area_vertical = p.windowHeight - obstacle10_height / 2
@@ -110,10 +110,10 @@ const _app = new p5(p5Instance => {
   let coin1_width = 35
   let coin1_height = 35
   let spawntrigger_coin1 = true
-  let rnd_number_coin1 = p.random(10, 100)
+  let rnd_number_coin1 = p.random(200, 300)
   let speed_coin1 = 5
   let coin1_spawn_pos_horizontal = p.windowWidth + coin1_width + rnd_number_coin1 * 2
-  let coin1_spawn_area_vertical = p.windowHeight - coin1_height / 2
+  let coin1_spawn_area_vertical = p.windowHeight - coin1_height
   let coin1_pos_horizontal = coin1_spawn_pos_horizontal
   let coin1_pos_vertical = getRndInteger(0 - coin1_height / 2, coin1_spawn_area_vertical)
 
@@ -121,7 +121,7 @@ const _app = new p5(p5Instance => {
   let deathscreen_width = p.windowWidth
   let deathscreen_height = p.windowHeight
   let deathscreen_pos_horizontal = 0
-  let deathscreen_pos_vertikal = 0
+  let deathscreen_pos_Vertical = 0
   let space = false
 
   //Menu
@@ -154,7 +154,7 @@ const _app = new p5(p5Instance => {
   let Player_Width = 90
   let Player_height = 90
   let Player_Position_Horizontal = p.windowWidth / 2 - Player_Width / 2
-  let Player_Position_Vertikal = p.windowHeight / 2 - Player_height / 2
+  let Player_Position_Vertical = p.windowHeight / 2 - Player_height / 2
   let lives = 3
   let speed = 6
   let up = false
@@ -169,8 +169,13 @@ const _app = new p5(p5Instance => {
   let flash_height = 80
   let flash_pos_horizontal = p.windowWidth / 2 - Player_Width / 2 - 250
   let flash_pos_vertical = p.windowHeight / 2 - Player_height / 2 + 30
+  let coinmaster_width = 80
+  let coinmaster_height = 80
+  let coinmaster_pos_horizontal = p.windowWidth / 2 - Player_Width / 2
+  let coinmaster_pos_vertical = p.windowHeight / 2 - Player_height / 2 + 30
 
   let flash = false
+  let coinmaster = false
 
   //Randomizer
   function getRndInteger(min: any, max: any) {
@@ -207,6 +212,9 @@ const _app = new p5(p5Instance => {
       power_menu = false
       flash = true
     }
+    if (p.keyCode === 50)
+      power_menu = false
+    coinmaster = true
   }
 
   p.keyReleased = function () {
@@ -252,24 +260,28 @@ const _app = new p5(p5Instance => {
     if (spawntrigger10 == true) {
       obstacle10_pos_vertical = getRndInteger(0 - obstacle1_height / 2, obstacle1_spawn_area_vertical)
     }
+    if (spawntrigger_coin1 == true) {
+      coin1_pos_vertical = getRndInteger(0 - coin1_height / 2, coin1_spawn_area_vertical)
+    }
+
 
     //Background
     p.background(0, 0, 0)//img für bild
 
     //Player
     p.fill(160, 32, 240)
-    p.rect(Player_Position_Horizontal, Player_Position_Vertikal, Player_Width, Player_height)
+    p.rect(Player_Position_Horizontal, Player_Position_Vertical, Player_Width, Player_height)
     if (up == true) {
-      Player_Position_Vertikal = Player_Position_Vertikal - speed
+      Player_Position_Vertical = Player_Position_Vertical - speed
     }
     if (up == false) {
-      Player_Position_Vertikal = Player_Position_Vertikal + speed
+      Player_Position_Vertical = Player_Position_Vertical + speed
     }
-    if (Player_Position_Vertikal > p.windowHeight - Player_height) {
-      Player_Position_Vertikal = p.windowHeight - Player_height
+    if (Player_Position_Vertical > p.windowHeight - Player_height) {
+      Player_Position_Vertical = p.windowHeight - Player_height
     }
-    if (Player_Position_Vertikal < 0) {
-      Player_Position_Vertikal = 0
+    if (Player_Position_Vertical < 0) {
+      Player_Position_Vertical = 0
     }
 
     //Obstacle 1
@@ -394,7 +406,7 @@ const _app = new p5(p5Instance => {
 
     //Damage 1
     if (Player_Position_Horizontal + Player_Width > obstacle1_pos_horizontal + obstacle1_width && Player_Position_Horizontal < obstacle1_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height > obstacle1_pos_vertical + obstacle1_height && Player_Position_Vertikal < obstacle1_pos_vertical) {
+      if (Player_Position_Vertical + Player_height > obstacle1_pos_vertical + obstacle1_height && Player_Position_Vertical < obstacle1_pos_vertical) {
         obstacle1_pos_horizontal = p.windowWidth + obstacle1_width
         lives = lives - 1
         spawntrigger1 = true
@@ -404,7 +416,7 @@ const _app = new p5(p5Instance => {
     }
     //Damage 2
     if (Player_Position_Horizontal + Player_Width > obstacle2_pos_horizontal + obstacle2_width && Player_Position_Horizontal < obstacle2_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height > obstacle2_pos_vertical + obstacle2_height && Player_Position_Vertikal < obstacle2_pos_vertical) {
+      if (Player_Position_Vertical + Player_height > obstacle2_pos_vertical + obstacle2_height && Player_Position_Vertical < obstacle2_pos_vertical) {
         obstacle2_pos_horizontal = p.windowWidth + obstacle2_width
         lives = lives - 1
         spawntrigger2 = true
@@ -414,7 +426,7 @@ const _app = new p5(p5Instance => {
     }
     //Damage 3
     if (Player_Position_Horizontal + Player_Width > obstacle3_pos_horizontal + obstacle3_width && Player_Position_Horizontal < obstacle3_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height > obstacle3_pos_vertical + obstacle3_height && Player_Position_Vertikal < obstacle3_pos_vertical) {
+      if (Player_Position_Vertical + Player_height > obstacle3_pos_vertical + obstacle3_height && Player_Position_Vertical < obstacle3_pos_vertical) {
         obstacle3_pos_horizontal = p.windowWidth + obstacle3_width
         lives = lives - 1
         spawntrigger3 = true
@@ -424,7 +436,7 @@ const _app = new p5(p5Instance => {
     }
     //Damage 4
     if (Player_Position_Horizontal + Player_Width > obstacle4_pos_horizontal + obstacle4_width && Player_Position_Horizontal < obstacle4_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height > obstacle4_pos_vertical + obstacle4_height && Player_Position_Vertikal < obstacle4_pos_vertical) {
+      if (Player_Position_Vertical + Player_height > obstacle4_pos_vertical + obstacle4_height && Player_Position_Vertical < obstacle4_pos_vertical) {
         obstacle4_pos_horizontal = p.windowWidth + obstacle4_width
         lives = lives - 1
         spawntrigger4 = true
@@ -434,7 +446,7 @@ const _app = new p5(p5Instance => {
     }
     //Damage 5
     if (Player_Position_Horizontal + Player_Width > obstacle5_pos_horizontal + obstacle5_width && Player_Position_Horizontal < obstacle5_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height > obstacle5_pos_vertical + obstacle5_height && Player_Position_Vertikal < obstacle5_pos_vertical) {
+      if (Player_Position_Vertical + Player_height > obstacle5_pos_vertical + obstacle5_height && Player_Position_Vertical < obstacle5_pos_vertical) {
         obstacle5_pos_horizontal = p.windowWidth + obstacle5_width
         lives = lives - 1
         spawntrigger5 = true
@@ -444,7 +456,7 @@ const _app = new p5(p5Instance => {
     }
     //Damage 6
     if (Player_Position_Horizontal + Player_Width > obstacle6_pos_horizontal + obstacle6_width && Player_Position_Horizontal < obstacle6_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height > obstacle6_pos_vertical + obstacle6_height && Player_Position_Vertikal < obstacle6_pos_vertical) {
+      if (Player_Position_Vertical + Player_height > obstacle6_pos_vertical + obstacle6_height && Player_Position_Vertical < obstacle6_pos_vertical) {
         obstacle6_pos_horizontal = p.windowWidth + obstacle6_width
         lives = lives - 1
         spawntrigger6 = true
@@ -454,7 +466,7 @@ const _app = new p5(p5Instance => {
     }
     //Damage 7
     if (Player_Position_Horizontal + Player_Width > obstacle7_pos_horizontal + obstacle7_width && Player_Position_Horizontal < obstacle7_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height > obstacle7_pos_vertical + obstacle7_height && Player_Position_Vertikal < obstacle7_pos_vertical) {
+      if (Player_Position_Vertical + Player_height > obstacle7_pos_vertical + obstacle7_height && Player_Position_Vertical < obstacle7_pos_vertical) {
         obstacle7_pos_horizontal = p.windowWidth + obstacle7_width
         lives = lives - 1
         spawntrigger7 = true
@@ -464,7 +476,7 @@ const _app = new p5(p5Instance => {
     }
     //Damage 8
     if (Player_Position_Horizontal + Player_Width > obstacle8_pos_horizontal + obstacle8_width && Player_Position_Horizontal < obstacle8_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height > obstacle8_pos_vertical + obstacle8_height && Player_Position_Vertikal < obstacle8_pos_vertical) {
+      if (Player_Position_Vertical + Player_height > obstacle8_pos_vertical + obstacle8_height && Player_Position_Vertical < obstacle8_pos_vertical) {
         obstacle8_pos_horizontal = p.windowWidth + obstacle8_width
         lives = lives - 1
         spawntrigger8 = true
@@ -474,7 +486,7 @@ const _app = new p5(p5Instance => {
     }
     //Damage 9
     if (Player_Position_Horizontal + Player_Width > obstacle9_pos_horizontal + obstacle9_width && Player_Position_Horizontal < obstacle9_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height > obstacle9_pos_vertical + obstacle9_height && Player_Position_Vertikal < obstacle9_pos_vertical) {
+      if (Player_Position_Vertical + Player_height > obstacle9_pos_vertical + obstacle9_height && Player_Position_Vertical < obstacle9_pos_vertical) {
         obstacle9_pos_horizontal = p.windowWidth + obstacle9_width
         lives = lives - 1
         spawntrigger9 = true
@@ -484,7 +496,7 @@ const _app = new p5(p5Instance => {
     }
     //Damage 10
     if (Player_Position_Horizontal + Player_Width > obstacle10_pos_horizontal + obstacle10_width && Player_Position_Horizontal < obstacle10_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height > obstacle10_pos_vertical + obstacle10_height && Player_Position_Vertikal < obstacle10_pos_vertical) {
+      if (Player_Position_Vertical + Player_height > obstacle10_pos_vertical + obstacle10_height && Player_Position_Vertical < obstacle10_pos_vertical) {
         obstacle10_pos_horizontal = p.windowWidth + obstacle10_width
         lives = lives - 1
         spawntrigger10 = true
@@ -507,10 +519,14 @@ const _app = new p5(p5Instance => {
 
     //Collect Coin 1
     if (Player_Position_Horizontal + Player_Width > coin1_pos_horizontal + coin1_width && Player_Position_Horizontal < coin1_pos_horizontal) {
-      if (Player_Position_Vertikal + Player_height > coin1_pos_vertical + coin1_height && Player_Position_Vertikal < coin1_pos_vertical) {
+      if (Player_Position_Vertical + Player_height > coin1_pos_vertical + coin1_height && Player_Position_Vertical < coin1_pos_vertical) {
         coin1_pos_horizontal = p.windowWidth + coin1_width
-        coins = coins + 1
         spawntrigger_coin1 = true
+        if (coinmaster == true) {
+          coins = coins + 2
+        } else {
+          coins = coins + 1
+        }
       }
     }
 
@@ -530,9 +546,10 @@ const _app = new p5(p5Instance => {
     p.textAlign(p.LEFT)
     p.text("Press [ESC] for menu", 25, 30)
     p.text("Lives: " + lives, 25, 60)
-    p.text("Level: " + level, 25, 90)
-    p.text(coins + "/" + coins_goal + " Coins", 25, 120)
-    p.text("Time survived: " + real_time, 25, 150)
+    p.text(coins + "/" + coins_goal + " Coins", 25, 90)
+    p.text("Time survived: " + real_time, 25, 120)
+    p.textSize(45)
+    p.text("Level: " + level, p.windowWidth / 2 - 100, 50)
 
     //Menu
     if (menu == true) {
@@ -568,24 +585,35 @@ const _app = new p5(p5Instance => {
     } else {
       timer_active = true
       speed = 6
-      speed1 = 5
-      speed2 = 5
-      speed3 = 5
-      speed4 = 5
-      speed5 = 5
-      speed6 = 5
-      speed7 = 5
-      speed8 = 5
-      speed9 = 5
-      speed10 = 5
-      speed_coin1 = 5
+      speed1 = 5 + level
+      speed2 = 5 + level
+      speed3 = 5 + level
+      speed4 = 5 + level
+      speed5 = 5 + level
+      speed6 = 5 + level
+      speed7 = 5 + level
+      speed8 = 5 + level
+      speed9 = 5 + level
+      speed10 = 5 + level
+      speed_coin1 = 5  + level
     }
 
     //Coins/Level
-    if (coins == coins_goal) {
+    if (coins >= coins_goal) {
       coins = 0
       coins_goal = coins_goal * 2
       level = level + 1
+      speed1 = speed1 + 1
+      speed2 = speed2 + 1
+      speed3 = speed3 + 1
+      speed4 = speed4 + 1
+      speed5 = speed5 + 1
+      speed6 = speed6 + 1
+      speed7 = speed7 + 1
+      speed8 = speed8 + 1
+      speed9 = speed9 + 1
+      speed10 = speed10 + 1
+      speed_coin1 = speed_coin1 + 1
     }
 
     //Superpower
@@ -605,15 +633,18 @@ const _app = new p5(p5Instance => {
       speed_coin1 = 0
       p.fill(87, 87, 87)
       p.rect(power_menu_pos_horizontal, power_menu_pos_vertical, power_menu_width, power_menu_height)
+      p.fill(600, 50, 30)
+      p.rect(flash_pos_horizontal, flash_pos_vertical, flash_width, flash_height)
+      p.fill(205, 149, 12)
+      p.rect(coinmaster_pos_horizontal, coinmaster_pos_vertical, coinmaster_width, coinmaster_height)
       p.fill(255, 255, 255)
       p.textSize(60)
       p.text("Choose a Superpower", p.windowWidth / 2 - 255, p.windowHeight / 2 - 100)
-      p.fill(600, 50, 30)
-      p.rect(flash_pos_horizontal, flash_pos_vertical, flash_width, flash_height)
-      p.fill(255, 255, 255)
       p.textSize(20)
       p.text("Flash", p.windowWidth / 2 - 278, p.windowHeight / 2 + 105)
       p.text("[1]", p.windowWidth / 2 - 267, p.windowHeight / 2 + 32)
+      p.text("Coinmaster", p.windowWidth / 2 - 51, p.windowHeight / 2 + 105)
+      p.text("[2]", p.windowWidth / 2 - 16, p.windowHeight / 2 + 32)
     }
 
     if (flash == true) {
@@ -623,7 +654,7 @@ const _app = new p5(p5Instance => {
     //Death
     if (lives < 1) {
       p.fill(0, 0, 0)
-      p.rect(deathscreen_pos_horizontal, deathscreen_pos_vertikal, deathscreen_width, deathscreen_height)
+      p.rect(deathscreen_pos_horizontal, deathscreen_pos_Vertical, deathscreen_width, deathscreen_height)
       p.fill(600, 50, 30)
       p.textSize(90)
       p.textAlign(p.CENTER)
@@ -638,6 +669,7 @@ const _app = new p5(p5Instance => {
         real_time = 0
         coins = 0
         coins_goal = 5
+        level = 1
         power_menu = true
       }
     }
