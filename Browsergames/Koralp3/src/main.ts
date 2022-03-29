6848e3f2804ddee774c88db4d05bbc2612eecb59
roterkohlrabi <koralpayata@gmail.com>
2022-03-29 15:28:04 +0200

    jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj

--- Browsergames/Koralp3/src/main.ts
+++ Browsergames/Koralp3/src/main.ts
@@ -5,112 +5,51 @@ import './style.css';
 const app = new p5(p5Instance => {
   const p = p5Instance as unknown as p5;
 
-  let gamerunning = 1
-
-  let score = 1
-  let speed = 1
-  let colorspeed = 10
-
-  let positionX=100
-  let positionY=100
-
-  let sizeX=50
-  let sizeY=50
-
-  let positionX1=1000
-  let positionY1=1000
-
-  let sizeX1=5000
-  let sizeY1=500
-
-  let borderX=10000
-  let borderY=2000
-
-  let farbe1=50 //rot
-  let farbe2=50 //grün
-  let farbe3=200 //blau
+  let borderX = 400
+  let borderY = 400
 
 
 
-  let farbe4=100 //rot
-  var farbe5=200 //grün
-  let farbe6=100 //blau
+
+
+  let BG1 = 200
+  let BG2 = 200
+  let BG3 = 200
+
+  let P1 = 100
+  let P2 = 100
+  let P3 = 100
+
+  let O1 = 0
+  let O2 = 0
+  let O3 = 0
+
+  let PpX = 100
+  let PpY = 100
+
+  let PsX = 100
+  let PsY = 100
   
-  let ßp=80
+  
+  
+  var BGcolor = p.background(BG1, BG2, BG3)
+  var playercolor = p.fill(P1, P2, P3)
+  var obstaclecolor = p.fill(O1, O2, O3)
 
-  let ßw=87
-  let ßa=65
-  let ßs=83
-  let ßd=68
-  let ßpfeiltasterechts=39
-  let ßpfeiltastelinks=37
-  let ßpfeiltasteoben=38
-  let ßpfeiltasteunten=40
+  var BGsize = p.createCanvas(borderX=borderX-1, borderY=borderY-1)
+  var playersize = p.rect(PpX, PpY, PsX, PsY)
 
-  let ßr=82
-
-  let ßzirkumflex=220
-
+    
   p.setup = function setup() {
-    p.createCanvas(borderX=borderX-1, borderY=borderY-1);
-  };
-
-  p.draw = function draw() {
-    var roundedSpeed = Math.round(speed) * 1;
-
-    var speedanzeige = document.getElementById("speedanzeige");
-    speedanzeige!.innerText = "speed ~ " + roundedSpeed.toString();
-
-
-    var roundedScore = Math.round(score) * 1;
-
-    var scorevalue = document.getElementById("score");
-    scorevalue!.innerText = "score: " + roundedScore.toString();
-
-
-    
-
-
-    p.background(farbe1, farbe2, farbe3);
-    p.fill(farbe4,farbe5,farbe6);
-    p.rotate(0.0)
-
-    p.rect(positionX1, positionY1, sizeX1, sizeY1)
-    p.rect(positionX, positionY, sizeX, sizeY)
-
-
-    if (p.keyIsDown(ßp)) {alert("Your game is paused. Press OK to unpause.");}
-
-    if (p.keyIsDown(ßd) && positionX<borderX-sizeX+speed && positionY<borderY-sizeY+speed && positionX>0-speed && positionY>0-speed) {positionX = positionX + speed}
-    if (p.keyIsDown(ßs) && positionX<borderX-sizeX+speed && positionY<borderY-sizeY+speed && positionX>0-speed && positionY>0-speed) {positionY = positionY + speed}
-    if (p.keyIsDown(ßa) && positionX<borderX-sizeX+speed && positionY<borderY-sizeY+speed && positionX>0-speed && positionY>0-speed) {positionX = positionX - speed}
-    if (p.keyIsDown(ßw) && positionX<borderX-sizeX+speed && positionY<borderY-sizeY+speed && positionX>0-speed && positionY>0-speed) {positionY = positionY - speed}
-
-    if (p.keyIsDown(ßpfeiltasterechts)) {farbe4 = farbe4 +colorspeed; farbe5 = farbe5 +colorspeed; farbe6 = farbe6 +colorspeed}
-    if (p.keyIsDown(ßpfeiltastelinks)) {farbe4 = farbe4 -colorspeed; farbe5 = farbe5 -colorspeed; farbe6 = farbe6 -colorspeed}
-    if (p.keyIsDown(ßpfeiltasteoben)) {farbe4 = farbe4 +colorspeed; farbe5 = farbe5 +colorspeed; farbe6 = farbe6 +colorspeed}
-    if (p.keyIsDown(ßpfeiltasteunten)) {farbe4 = farbe4 -colorspeed; farbe5 = farbe5 -colorspeed; farbe6 = farbe6 -colorspeed}
-
-    if (borderX > -1000 && gamerunning > 0) {speed=speed+speed*0.002 ; score=Math.pow(1.002,speed); positionX1=positionX1-speed}
-    if (borderX > -1000) {console.log(gamerunning); }
-    
-    const YouDied = "You died! Refresh to play again!"
-    
-    if (positionX<0-speed && p.keyIsDown(ßa)) {alert(YouDied);(gamerunning = gamerunning -2)}
-    if (positionX>borderX-sizeX+speed && p.keyIsDown(ßd)) {alert(YouDied);(gamerunning = gamerunning -2)}
-    if (positionY<0-speed && p.keyIsDown(ßw)) {alert(YouDied);(gamerunning = gamerunning -2)} 
-    if (positionY>borderY-sizeY+speed && p.keyIsDown(ßs)) {alert(YouDied);(gamerunning = gamerunning -2)}
-
-    if (speed == Infinity) {alert("WTF ARE YOU DOING ???!!!")}
+    p.createCanvas(borderX, borderY);
   };
 
 
-  p.keyPressed = () => {
-    console.log(p.keyCode);
-    
-  
-    
-  }
+  p.draw = function draw() {BGcolor;playercolor;p.rotate(0.0)}
 
 
-}, document.getElementById('app')!);
+
+
+
+
+})
