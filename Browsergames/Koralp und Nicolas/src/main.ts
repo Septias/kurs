600069aaa8874bfb6f12c2b552153352996e4869
BuildTools <unconfigured@null.spigotmc.org>
2022-06-02 13:26:11 +0200

    test lol

--- Browsergames/Koralp und Nicolas/src/main.ts
+++ Browsergames/Koralp und Nicolas/src/main.ts
@@ -1,21 +1,27 @@
 import p5 from 'p5';
-
 import './style.css';
 
-
-const app = new p5(p5Instance => {
+new p5(p5Instance => {
   const p = p5Instance as unknown as p5;
 
 
-
   p.setup = function setup() {
-    p.createCanvas(500,500);
+    p.createCanvas(10,10)
+    p.createCanvas(10,10)
+    p.background("red")
   };
 
+  p.keyPressed = () => {
+
+  }
+
+
   p.draw = function draw() {
-    
-    }
+    p.background(0);
 
 
+
+  };
 
 }, document.getElementById('app')!);
+
