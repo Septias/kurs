6f1418ec6a2576f64d9050011989c0c35de146a3
roterkohlrabi <koralpayata@gmail.com>
2022-06-23 13:56:23 +0200

    moin

--- Browsergames/Koralp und Nicolas/src/main.ts
+++ Browsergames/Koralp und Nicolas/src/main.ts
@@ -5,9 +5,22 @@ new p5(p5Instance => {
   const p = p5Instance as unknown as p5;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
   p.setup = function setup() {
-    p.createCanvas(10,10)
-    p.background(255, 0, 0)
+    p.createCanvas(400,400)
+    
   };
 
   p.keyPressed = () => {
@@ -16,17 +29,15 @@ new p5(p5Instance => {
 
 
   p.draw = function draw() {
-    p.background(0);
+    p.background(0,255,0);
 
+    p.fill(100, 100, 100)
+    p.rect(200, 0, 50, 50)
+  }
 
-
+    console.log(this.keyPressed)
   };
 
-}, document.getElementById('app')!);
+ document.getElementById('app')!);
 
 
-
-
-
-
-FFFFFFFFF
