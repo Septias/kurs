3f230f29a967409468d95d0da8ac30a2db6614ba
Blaze-Codes <nic.holzmann@web.de>
2022-06-23 13:55:28 +0200

    rfjuor3bger23r

--- Browsergames/Koralp und Nicolas/src/main.ts
+++ Browsergames/Koralp und Nicolas/src/main.ts
@@ -4,21 +4,42 @@ import './style.css';
 new p5(p5Instance => {
   const p = p5Instance as unknown as p5;
 
-
+  var x = 100
+  var y = 100
+  var up = false
   p.setup = function setup() {
-    p.createCanvas(100 ,100)
+    p.createCanvas(p.windowWidth,p.windowHeight)
   };
 
-  p.keyPressed = () => {
 
-  }
+    p.keyPressed = () => {
+      if (p.keyCode == 32) {
+        up = true
+      }
+      
+    }
 
+    p.keyReleased = () => {
+      if (p.keyCode == 32){
+      up = false
+    }
+    }
+
+  
+    
 
   p.draw = function draw() {
-    p.background(0,255,0);
+    p.background(255,255,0);
+    p.fill(255,0,0)
+      p.rect(x,y,100,100)
 
-
+    if (up == true){
+      y = y-10
+    }else y = y+10
 
   };
 
 }, document.getElementById('app')!);
+
+
+// space ist 32
