aa0d530413232eda6685f51c76f957f17f018475
Plsstayfriendly <Bentik@gmx.de>
2022-06-30 13:33:46 +0200

    use veu

--- DeltaChat Games/hangman_vue/src/components/hangman.vue
+++ DeltaChat Games/hangman_vue/src/components/hangman.vue
@@ -1,150 +1,132 @@
-<template lang="pug">
-div(ref="canvas")
-
-</template>
-
 <script lang="ts" setup>
-import p5 from 'p5';
+/* eslint-disable new-cap */
+import p5 from 'p5'
 
-let canvas: HTMLElement = $ref()
+const canvas: HTMLElement = $ref()
 
-enum SCREENS {
-  gameScreen,
-  endScreen
+function randomword(words: string[]) {
+  return words[Math.floor(Math.random() * (words.length - 1))]
+}
+
+const words = ['hallo', 'tschüss']
+let target_word = randomword(words)
+let new_string = $ref('_ '.repeat(target_word.length))
+
+enum GameState {
+  Playing,
+  Won,
+  Lost,
+}
+
+let game_state = $ref(GameState.Playing)
+
+const body_x_top = 420
+const body_y_top = 320
+const galgen_x_bottom = body_x_top - 250
+const galgen_y_bottom = body_y_top + 300
+let elements_to_draw = 0
+
+const parts = [
+  // galgen
+  [galgen_x_bottom, galgen_y_bottom, 250, 250, 3.1415, 0],
+  [galgen_x_bottom, galgen_y_bottom - 250 / 2, galgen_x_bottom, galgen_y_bottom - 450],
+  [galgen_x_bottom, galgen_y_bottom - 450, body_x_top, galgen_y_bottom - 450],
+  [galgen_x_bottom, galgen_y_bottom - 370, galgen_x_bottom + 80, galgen_y_bottom - 450],
+  [body_x_top, galgen_y_bottom - 450, body_x_top, body_y_top - 50],
+  // head
+  [body_x_top, body_y_top - 25, 50],
+  [body_x_top - 1 / 3 * 25, body_y_top - 28, 2],
+  [body_x_top + 1 / 3 * 25, body_y_top - 28, 2],
+  // body
+  [body_x_top, body_y_top, body_x_top, body_y_top + 100],
+  // arms
+  [body_x_top, body_y_top + 15, body_x_top - 30, body_y_top + 90],
+  [body_x_top, body_y_top + 15, body_x_top + 30, body_y_top + 90],
+  // legs
+  [body_x_top, body_y_top + 100, body_x_top - 25, body_y_top + 175],
+  [body_x_top, body_y_top + 100, body_x_top + 25, body_y_top + 175],
+]
+
+let letters = {} as { [key: string]: boolean | undefined }
+let parts_to_draw = [parts[0]] as number[][]
+
+function replay() {
+  console.log('starting new game')
+  letters = {}
+  parts_to_draw = []
+  target_word = randomword(words)
+  game_state = GameState.Playing
 }
 
 onMounted(() => {
-
-  new p5(p5Instance => {
-    const p = p5Instance as unknown as p5;
-
+  // eslint-disable-next-line no-new
+  new p5((p5Instance) => {
+    const p = p5Instance as unknown as p5
     p.setup = function setup() {
-      p.createCanvas(100, 500);
-      p.textFont('Arial');
+      p.createCanvas(400, 500)
+      p.textFont('Arial')
       p.textSize(30)
       p.frameRate(60)
-      p.background(255, 255, 255);
-    };
-
-    const body_x_top = 50
-    const body_y_top = 250
-    const galgen_x_bottom = body_x_top - 250
-    const galgen_y_bottom = body_y_top + 300
-    const words = ["hallo", "tschüss"]
-    let target_word = words[Math.floor(Math.random() * (words.length - 1))]
-    let new_string = "_ ".repeat(target_word.length)
-    let elements_to_draw = 0
-    let render_function = SCREENS.gameScreen
-
-    const parts = [
-      //galgen
-      [galgen_x_bottom, galgen_y_bottom, 250, 250, p.PI, 0],
-      [galgen_x_bottom, galgen_y_bottom - 250 / 2, galgen_x_bottom, galgen_y_bottom - 450],
-      [galgen_x_bottom, galgen_y_bottom - 450, body_x_top, galgen_y_bottom - 450],
-      [galgen_x_bottom, galgen_y_bottom - 370, galgen_x_bottom + 80, galgen_y_bottom - 450],
-      [body_x_top, galgen_y_bottom - 450, body_x_top, body_y_top - 50],
-      //head
-      [body_x_top, body_y_top - 25, 50],
-      [body_x_top - 1 / 3 * 25, body_y_top - 28, 2],
-      [body_x_top + 1 / 3 * 25, body_y_top - 28, 2],
-      //body
-      [body_x_top, body_y_top, body_x_top, body_y_top + 100],
-      //arms
-      [body_x_top, body_y_top + 15, body_x_top - 30, body_y_top + 90],
-      [body_x_top, body_y_top + 15, body_x_top + 30, body_y_top + 90],
-      //legs
-      [body_x_top, body_y_top + 100, body_x_top - 25, body_y_top + 175],
-      [body_x_top, body_y_top + 100, body_x_top + 25, body_y_top + 175],
-    ]
-    const letters = {
-      a: false,
-      b: false,
-      c: false,
-      d: false,
-      e: false,
-      f: false,
-      g: false,
-      h: false,
-      i: false,
-      j: false,
-      k: false,
-      l: false,
-      m: false,
-      n: false,
-      o: false,
-      p: false,
-      q: false,
-      r: false,
-      s: false,
-      t: false,
-      u: false,
-      v: false,
-      w: false,
-      x: false,
-      y: false,
-      z: false,
-    } as { [key: string]: boolean }
-
-    const parts_to_draw = [parts[0]] as number[][]
+      p.background(255, 255, 255)
+    }
 
     p.draw = function draw() {
       p.background(255)
-
+      p.scale(0.7)
       for (const part of parts_to_draw) {
-        if (part.length == 6) {
+        if (part.length === 6)
           p.arc(part[0], part[1], part[2], part[3], part[4], part[5])
-        }
-        if (part.length == 4) {
-          p.line(part[0], part[1], part[2], part[3])
-        }
-        if (part.length == 3) {
-          p.circle(part[0], part[1], part[2])
-        }
 
-        p.text(new_string, p.windowWidth / 2, p.windowHeight / 2 + 400)
+        if (part.length === 4)
+          p.line(part[0], part[1], part[2], part[3])
+
+        if (part.length === 3)
+          p.circle(part[0], part[1], part[2])
       }
     }
 
     p.keyPressed = () => {
-      let key = p.key
-      let keyCode = p.keyCode
+      const key = p.key
+      const keyCode = p.keyCode
 
-      if (!(65 <= keyCode && keyCode <= 90 || 97 <= keyCode && keyCode <= 122)) {
+      if (!((keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122)))
         return
-      }
 
-      letters[key] = true
-
-      if (!target_word.includes(key)) {
+      if (!target_word.includes(key) && letters[key] != true) {
         elements_to_draw++
         parts_to_draw.push(parts[elements_to_draw])
       }
+      letters[key] = true
 
-      new_string = ""
-      let comparison_string = ""
+      new_string = ''
+      let comparison_string = ''
       for (const letter of target_word) {
-        if (letters[letter] == true) {
+        if (letters[letter] === true) {
           new_string += letter
           comparison_string += letter
-        } else {
-          new_string += "_"
         }
-        new_string += " "
+        else {
+          new_string += '_'
+        }
+        new_string += ' '
       }
 
-      if (target_word == comparison_string) {
+      if (comparison_string === target_word)
+        game_state = GameState.Won
 
-
-        const timeout = setTimeout(timeout_endscreen, 1000)
-        function timeout_endscreen() {
-
-          render_function = SCREENS.endScreen
-        }
-      }
+      if (parts_to_draw.length === parts.length)
+        game_state = GameState.Lost
     }
-  }, canvas);
-
-
+  }, canvas)
 })
-
 </script>
+
+<template lang="pug">
+div
+  p.text-center.text-red-500.font.mt-3(v-if="game_state === GameState.Lost") Du died
+  p.text-center.text-green-500.text-bold.mt-3(v-if="game_state === GameState.Won") Mission passed
+  div(ref="canvas")
+  p.text-center.p-4.text-3xl {{new_string}}
+  div.flex.justify-center
+    button.bg-pink-500.text-2xl.p-5.rounded.text-white.leading-none(v-if="!(game_state == GameState.Playing)" @click="replay") Replay
+</template>
--- DeltaChat Games/hangman_vue/src/pages/index.vue
+++ DeltaChat Games/hangman_vue/src/pages/index.vue
@@ -5,6 +5,6 @@
 <template lang="pug">
 main.flex.justify-center.items-center.h-screen
   div
-    h1.text-pink-500.text-6xl Hangman
-      hangman
+    h1.text-pink-500.text-6xl.mb-2.text-center Hangman
+    hangman
 </template>
